03276d7588341614b47881f5fdc866619865bc11
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-03 20:49:12 -0300

    Finished mobile css

---
+++ script.js
@@ -0,0 +1,9 @@
+/* 
+.div15 { grid-area: 5 / 1 / 6 / 2; } monday
+.div16 { grid-area: 3 / 2 / 4 / 3; } tuesday
+.div17 { grid-area: 1 / 3 / 2 / 4; } wednesday
+.div18 { grid-area: 3 / 4 / 4 / 5; } thursday
+.div19 { grid-area: 4 / 5 / 5 / 6; } friday
+.div20 { grid-area: 2 / 6 / 3 / 7; } saturday
+.div21 { grid-area: 4 / 7 / 5 / 8; } sunday
+*/
