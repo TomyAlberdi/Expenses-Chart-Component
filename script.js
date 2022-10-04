59a737cee035b28b3a50a6879ae0ea610ee970d3
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-04 19:34:05 -0300

    Finished challenge

--- script.js
+++ script.js
@@ -7,3 +7,42 @@
 .div20 { grid-area: 2 / 6 / 3 / 7; } saturday
 .div21 { grid-area: 4 / 7 / 5 / 8; } sunday
 */
+
+const graph = document.querySelector('.graph')
+
+const mon = document.getElementById('0')
+const tue = document.getElementById('1')
+const wed = document.getElementById('2')
+const thu = document.getElementById('3')
+const fry = document.getElementById('4')
+const sat = document.getElementById('5')
+const sun = document.getElementById('6')
+const dayArr = [mon,tue,wed,thu,fry,sat,sun]
+
+fetch('./data.json')
+.then((res) => res.json())
+.then((data) => getData(data))
+let getData = data => {
+    dayArr.forEach(d => {
+        d.addEventListener('click', e => {
+            removeSpend()
+            createSpend(data,d.id)
+        })
+    })
+}
+
+let createSpend = (data,id) => {
+    let divP = document.createElement('div')
+    let idn = parseInt(id,10)
+    divP.classList.add(`div${idn+15}`)
+    divP.classList.add(`day`)
+    divP.innerHTML = "$" + data[id].amount
+    graph.appendChild(divP)
+}
+
+let removeSpend = () => {
+    let sp = document.querySelector('.day')
+    if (sp !== null) {
+        sp.remove()
+    }
+}
